6e83a5030ae85d946a44d86471c9f3ae46a258e7
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-08-03 16:01:20 +0300

    feat: add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/google-calendar-clone/",
   server: {
     port: 3000,
   },
